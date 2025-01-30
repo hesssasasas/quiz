7d8c8f8e30b7ebc9416af2d93d0df57b172f784e
hessa <h_nor-ariwwin@biswashington.org>
2025-01-30 17:31:33 +0000

    done

--- main.py
+++ main.py
@@ -1,1 +1,89 @@
-#Ask the quiz
+# Ask the quiz question
+question = "What is the capital of France?"
+print(question)
+
+# Take the user's answer as input
+user_answer = input("Your answer: ")
+
+# Check if the answer is correct
+if user_answer.lower() == "paris" :
+    print("Correct!")
+else:
+    print("Oops! The correct answer is Paris.")
+
+score = 0
+score += 1
+# Display the score
+print(f"Your score is: {score}")
+
+ #Ask the quiz question
+question = "Where is China located in?"
+print(question)
+
+# Take the user's answer as input
+user_answer = input("Your answer: ")
+
+# Check if the answer is correct
+if user_answer.lower() == "Asia" :
+    print("Correct!")
+else:
+    print("Oops! The correct answer is Asia.")
+
+score = 0
+score += 1
+# Display the score
+print(f"Your score is: {score}")
+
+# Ask the quiz question
+question = "Where is macarons from?"
+print(question)
+
+# Take the user's answer as input
+user_answer = input("Your answer: ")
+
+# Check if the answer is correct
+if user_answer.lower() == "paris" :
+    print("Correct!")
+else:
+    print("Oops! The correct answer is Paris.")
+
+score = 0
+score += 1
+# Display the score
+print(f"Your score is: {score}")
+
+# Ask the quiz question
+question = "What country is England?"
+print(question)
+
+# Take the user's answer as input
+user_answer = input("Your answer: ")
+
+# Check if the answer is correct
+if user_answer.lower() == "Europe" :
+    print("Correct!")
+else:
+    print("Oops! The correct answer is Europe.")
+
+score = 0
+score += 1
+# Display the score
+print(f"Your score is: {score}")
+
+# Ask the quiz question
+question = "What is the capital of China?"
+print(question)
+
+# Take the user's answer as input
+user_answer = input("Your answer: ")
+
+# Check if the answer is correct
+if user_answer.lower() == "Beijing" :
+    print("Correct!")
+else:
+    print("Oops! The correct answer is Beijing.")
+
+score = 0
+score += 1
+# Display the score
+print(f"Your score is: {score}")
